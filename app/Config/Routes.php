7a3ea4f737b1efe04a91e2097a8a7a72eb920c02
GallanSJK <gallansteffan99@gmail.com>
2023-11-29 03:35:26 +0700

    add payment and edit profile

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,6 +11,8 @@ $routes->get('/login', 'Login::index');
 $routes->get('/login-process', 'Login::login_process');
 $routes->get('/logout', 'Login::logout');
 $routes->get('/topup', 'Topup::index');
+$routes->get('/payment/(:any)', 'Payment::index/$1');
 $routes->get('/transaction', 'Transaction::index');
 $routes->get('/transaction-show-more', 'Transaction::list');
 $routes->get('/account', 'Account::index');
+$routes->get('/edit-profile', 'Account::edit');
